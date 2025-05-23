a18b808c09213689b221b2148896eafef2d749a2
Dev João Victor Pegoretti <143030410+joaovictorPegoretti@users.noreply.github.com>
2025-05-23 22:55:58 +0000

    Adicionado Exercicio das cores

--- Public/index.php
+++ Public/index.php
@@ -3,14 +3,16 @@
 
 Class Aluno{
     public int $IdadeAluno;
+    public array $Cores;
 
- public function __construct(int $Idade) {
-        $this->idade = $Idade;
+ public function __construct(int $idadeAluno, array $cores) {
+        $this->idade = $idadeAluno;
+        $this->Cor = $cores;
  }
 
 }
 
-$aluno = new Aluno(19);
+$aluno = new Aluno(19, ["vermelho", "verde", "azul", "amarelo", "roxo"]);
 
 
 if($aluno->idade >= 18){
@@ -18,6 +20,11 @@ if($aluno->idade >= 18){
 }
 else{
     echo "<p>O aluno não é maior de idade</p>";
+}
+
+echo "<p>Lista de cores</p>";
+foreach($aluno->Cor as $lista){
+    echo "<p>Cores:</p>" . $lista;
 }
 
 ?>
