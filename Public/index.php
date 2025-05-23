1821ea2704b78d238cb0d6e4659eed4fcfed4a85
Dev João Victor Pegoretti <143030410+joaovictorPegoretti@users.noreply.github.com>
2025-05-23 23:01:05 +0000

    Exercicio 3 até quest 2

--- Public/index.php
+++ Public/index.php
@@ -15,17 +15,21 @@ Class Aluno{
 $aluno = new Aluno(19, ["vermelho", "verde", "azul", "amarelo", "roxo"]);
 
 
+#region Exercicio 3 - Quest 1
 if($aluno->idade >= 18){
     echo "<p>O aluno é maior de idade</p>";
 }
 else{
     echo "<p>O aluno não é maior de idade</p>";
 }
+#endregion
 
+#region Exercicio 3 - Quest 2
 echo "<p>Lista de cores</p>";
 foreach($aluno->Cor as $lista){
-    echo "<p>Cores:</p>" . $lista;
+    echo "<p>Cores: $lista</p>";
 }
+#endregion
 
 ?>
 
